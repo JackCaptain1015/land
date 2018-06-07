70e5d00c5fe50f599b82c2bcafbeb470a966bd6d
jackCaptain1015 <15700094730@163.com>
2018-06-07 17:17:59 +0800

    commit

---
+++ land-dal/src/main/java/com/captain/template/segment/FieldSegment.java
@@ -0,0 +1,22 @@
+package com.captain.template.segment;
+
+import java.sql.Connection;
+
+/**
+ * Created by wurz on 2018/6/7.
+ */
+public class FieldSegment {
+    private Connection connection;
+
+    public FieldSegment(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void getField(){
+
+    }
+
+    public void getFieldSql(){
+
+    }
+}
