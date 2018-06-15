bdeea9e1a9e6493c23028311cb7d9daa7eabc7ac
jackCaptain1015 <15700094730@163.com>
2018-06-15 09:36:49 +0800

    commit

--- land-dal/src/main/java/com/captain/template/TableSpider.java
+++ land-dal/src/main/java/com/captain/template/TableSpider.java
@@ -2,13 +2,17 @@ package com.captain.template;
 
 import com.captain.template.bean.Field;
 import com.captain.template.enums.SqlSegmentEnums;
+import com.captain.template.generate.GenerateMapperXml;
 import com.captain.template.segment.FieldSegment;
+import com.captain.template.utils.FileUtils;
 import com.captain.template.utils.StringUtils;
 import com.google.common.collect.Maps;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
@@ -18,264 +22,21 @@ import java.util.Map;
  */
 @Component
 public class TableSpider {
-    public static final Map<String,String> JAVA_TYPE_MAP = Maps.newHashMap();
-
-    static{
-        /**
-         * 字符串
-         */
-        JAVA_TYPE_MAP.put("string","VARCHAR");
-        /**
-         * 浮点数
-         */
-        JAVA_TYPE_MAP.put("bigdecimal","DECIMAL");
-        JAVA_TYPE_MAP.put("float","DECIMAL");
-        /**
-         * 数字
-         */
-        JAVA_TYPE_MAP.put("boolean","BIT");
-        JAVA_TYPE_MAP.put("byte","TINYINT");
-        JAVA_TYPE_MAP.put("short","SMALLINT");
-        JAVA_TYPE_MAP.put("int","INTEGER");
-        JAVA_TYPE_MAP.put("integer","INTEGER");
-        JAVA_TYPE_MAP.put("long","BIGINT");
-        /**
-         * 日期
-         */
-        JAVA_TYPE_MAP.put("date","TIMESTAMP");
-
-    }
-
-    //key:tableName value.key:sql片段的名字 value.value:sql片段
-    private Map<String,Map<String,String>> tableSqlSegment = Maps.newConcurrentMap();
 
     @Resource
-    private FieldSegment fieldSegment;
-    @Value("${generate.mapperLocation}")
-    private String mapperLocation;
-    @Value("${generate.entityLocation}")
-    private String entityLocation;
-    @Value("${generate.serviceLocation}")
-    private String serviceLocation;
-    @Value("${generate.createTimeFieldName}")
-    private String createTimeFieldName;
-    @Value("${generate.modifyTimeFieldName}")
-    private String modifyTimeFieldName;
+    private GenerateMapperXml generateMapper;
 
 
-    public void generateSqlMapper() throws SQLException {
-        fieldSegment.init();
-        Map<String, String> mapperMap = this.generateMapper();
+    public void generateSqlMapper() throws SQLException, IOException {
+
+        Map<String, String> mapperMap = generateMapper.generateMapper();
+        
+        File mapperXml = FileUtils.createFile(FileUtils.class.getResource("").getPath());
 
     }
 
 
 
-    private Map<String,String> generateMapper(){
-        Map<String,String> tableMapperMap = Maps.newHashMap();
-        //生成代码片段
-        Map<String, String> resultMapSqlMap = this.generateResultMapSql();
-        Map<String, String> columnSqlMap = this.generateColumnSql();
-        Map<String, String> selectConditionSqlMap = this.generateSelectConditionSql();
-        Map<String, String> deleteIdSqlMap = this.generateDeleteIdSql();
-        Map<String, String> insertSqlMap = this.generateInsertSql();
-        Map<String, String> updateSqlMap = this.generateUpdateSql();
 
-        Map<String, List<Field>> tableFieldsMap = fieldSegment.getTableFieldsMap();
-        tableFieldsMap.forEach((tableName,fieldList) -> {
-            //拼接代码片段
-            StringBuffer jointSqlSegmentBuffer = new StringBuffer();
-            jointSqlSegmentBuffer.append(resultMapSqlMap.get(tableName)).append(columnSqlMap.get(tableName))
-                    .append(selectConditionSqlMap.get(tableName)).append(deleteIdSqlMap.get(tableName))
-                    .append(insertSqlMap.get(tableName)).append(updateSqlMap.get(tableName));
-
-            // TODO: 2018/6/12 往mapper里填充数据
-            String mapperSql = StringUtils.replaceSequenced(SqlSegmentEnums.MAPPER_SEG.getValue(),
-                    this.getMapperLocation(tableName),jointSqlSegmentBuffer.toString());
-            tableMapperMap.put(tableName,SqlSegmentEnums.HEAD_SEG.getValue()+mapperSql);
-        });
-
-        return tableMapperMap;
-    }
-
-    /**
-     *
-     * @return key:tableName value:ResultMap
-     */
-    private Map<String,String> generateResultMapSql(){
-        Map<String,String> tableResultMap = Maps.newHashMap();
-
-        Map<String, List<Field>> tableFieldsMap = fieldSegment.getTableFieldsMap();
-        tableFieldsMap.forEach((tableName,fieldList) -> {
-            StringBuffer resultMapSegSb = new StringBuffer();
-
-            fieldList.forEach((field) -> {
-                String resultSegEnumValue;
-                if ("id".equals(field.getFieldSourceName())){
-                    resultSegEnumValue = SqlSegmentEnums.RESULTMAP_RESULT_ID_SEG.getValue();
-                }else{
-                    resultSegEnumValue = SqlSegmentEnums.RESULTMAP_RESULT_SEG.getValue();
-                }
-                String resultSegStr = StringUtils.replaceSequenced(resultSegEnumValue, field.getFieldSourceName(),field.getFieldCamelName(),
-                        JAVA_TYPE_MAP.get(field.getFieldDescType().toLowerCase()));
-                resultMapSegSb.append(resultSegStr);
-
-            });
-
-            String resultMap = StringUtils.replaceSequenced(SqlSegmentEnums.RESULTMAP_SEG.getValue(),
-                    this.getEntityLocation(tableName), resultMapSegSb.toString());
-            tableResultMap.put(tableName,resultMap);
-        });
-        return tableResultMap;
-    }
-
-    /**
-     *
-     * @return key:tableName value:columnSql
-     */
-    private Map<String,String> generateColumnSql(){
-        Map<String,String> tableResultMap = Maps.newHashMap();
-
-        Map<String, List<Field>> tableFieldsMap = fieldSegment.getTableFieldsMap();
-        tableFieldsMap.forEach((tableName,fieldList) ->{
-            String allFieldNamStr = this.getColumnSql(fieldList);
-            String columnSql = StringUtils.replaceSequenced(SqlSegmentEnums.SQL_TAG_SEG.getValue(), allFieldNamStr);
-            tableResultMap.put(tableName,columnSql);
-        });
-        return tableResultMap;
-    }
-
-    /**
-     *
-     * @return key:tableName value:selectConditionSql
-     */
-    private Map<String,String> generateSelectConditionSql(){
-        Map<String,String> tableResultMap = Maps.newHashMap();
-
-        Map<String, List<Field>> tableFieldsMap = fieldSegment.getTableFieldsMap();
-        tableFieldsMap.forEach((tableName,fieldList) ->{
-
-            StringBuffer fieldConditionSql = new StringBuffer();
-            fieldList.forEach(field -> {
-                fieldConditionSql.append("<if test=\"").append(field.getFieldCamelName()).append(" != null\" >\n and")
-                        .append(field.getFieldSourceName()).append(" = #{").append(field.getFieldCamelName()).append("}").append(",\n</if>");
-            });
-            String selectConditionSql = StringUtils.replaceSequenced(SqlSegmentEnums.SELECT_CONDITION_TAG_SEG.getValue(),
-                    this.getEntityLocation(tableName), tableName, fieldConditionSql.toString());
-            tableResultMap.put(tableName,selectConditionSql);
-        });
-        return tableResultMap;
-    }
-
-    private Map<String,String> generateDeleteIdSql(){
-        Map<String,String> tableResultMap = Maps.newHashMap();
-
-        Map<String, List<Field>> tableFieldsMap = fieldSegment.getTableFieldsMap();
-        tableFieldsMap.forEach((tableName,fieldList) -> {
-            for (Field field : fieldList) {
-                if ("id".equals(field.getFieldSourceName())){
-                    String deleteIdSql = StringUtils.replaceSequenced(SqlSegmentEnums.DELETE_ID_TAG_SEG.getValue(),field.getFieldDescType(), tableName);
-                    tableResultMap.put(tableName,deleteIdSql);
-                    break;
-                }
-            }
-        });
-        return tableResultMap;
-    }
-
-    /**
-     * 生成insertSql
-     * @return
-     */
-    private Map<String,String> generateInsertSql(){
-        Map<String,String> tableResultMap = Maps.newHashMap();
-
-        Map<String, List<Field>> tableFieldsMap = fieldSegment.getTableFieldsMap();
-        tableFieldsMap.forEach((tableName,fieldList) -> {
-
-            StringBuffer insertColumnSqlBuffer = new StringBuffer();
-            StringBuffer insertValueSqlBuffer = new StringBuffer();
-            fieldList.forEach(field -> {
-                boolean isCreateTimeExist = createTimeFieldName != null && createTimeFieldName.length() != 0 && createTimeFieldName.equals(field.getFieldSourceName());
-                boolean isModifyTimeExist = modifyTimeFieldName != null && modifyTimeFieldName.length() != 0 && modifyTimeFieldName.equals(field.getFieldSourceName());
-                if (isCreateTimeExist || isModifyTimeExist){
-                    insertColumnSqlBuffer.append(field.getFieldSourceName()).append(",");
-                    insertValueSqlBuffer.append("now(),");
-                }else {
-                    insertColumnSqlBuffer.append(this.getIfTagSql(field.getFieldSourceName(),SqlSegmentEnums.INSERT_COLUMN_IF_TAG_SEG.getKey()));
-                    insertValueSqlBuffer.append(this.getIfTagSql(field.getFieldSourceName(),SqlSegmentEnums.INSERT_IF_TAG_SEG.getKey()));
-                }
-            });
-            String insertSql = StringUtils.replaceSequenced(SqlSegmentEnums.INSERT_TAG_SEG.getValue(), entityLocation, tableName, insertColumnSqlBuffer.toString(), insertValueSqlBuffer.toString());
-
-            tableResultMap.put(tableName,insertSql);
-        });
-        return tableResultMap;
-    }
-
-    private Map<String,String> generateUpdateSql(){
-        Map<String,String> tableResultMap = Maps.newHashMap();
-
-        Map<String, List<Field>> tableFieldsMap = fieldSegment.getTableFieldsMap();
-
-
-        tableFieldsMap.forEach((tableName,fieldList) -> {
-
-            StringBuffer updateSqlBuffer = new StringBuffer();
-            fieldList.forEach(field -> {
-                boolean isCreateTimeExist = createTimeFieldName != null && createTimeFieldName.length() != 0 && createTimeFieldName.equals(field.getFieldSourceName());
-                boolean isModifyTimeExist = modifyTimeFieldName != null && modifyTimeFieldName.length() != 0 && modifyTimeFieldName.equals(field.getFieldSourceName());
-                if (isCreateTimeExist){
-                    updateSqlBuffer.append("");
-                }else if(isModifyTimeExist){
-                    updateSqlBuffer.append(field.getFieldSourceName()).append(" = now(),\n");
-                }else{
-                    updateSqlBuffer.append(this.getIfTagSql(field.getFieldSourceName(),SqlSegmentEnums.UPDATE_IF_TAG_SEG.getKey()));
-                }
-            });
-            String updateSql = StringUtils.replaceSequenced(SqlSegmentEnums.UPDATE_TAG_SEG.getValue(), entityLocation, tableName, updateSqlBuffer.toString());
-
-            tableResultMap.put(tableName,updateSql);
-        });
-        return tableResultMap;
-    }
-
-    /**
-     * 比如id,name
-     * @return
-     */
-    private String getColumnSql(List<Field> fieldList){
-        StringBuffer allFieldNameBuffer = new StringBuffer();
-        fieldList.forEach(field -> {
-            allFieldNameBuffer.append(field.getFieldSourceName()).append(",");
-        });
-        allFieldNameBuffer.deleteCharAt(allFieldNameBuffer.length()-1);
-        return allFieldNameBuffer.toString();
-    }
-
-    /**
-     * <if>标签sql
-     * @return
-     */
-    private String getIfTagSql(String sourceColumnName,String ifTagType){
-        if (SqlSegmentEnums.INSERT_COLUMN_IF_TAG_SEG.getKey().equals(ifTagType)){
-            return StringUtils.replaceSequenced(SqlSegmentEnums.INSERT_COLUMN_IF_TAG_SEG.getValue(),StringUtils.underline2Camel(sourceColumnName,true),sourceColumnName);
-        }
-        if (SqlSegmentEnums.INSERT_IF_TAG_SEG.getKey().equals(ifTagType)){
-            return StringUtils.replaceSequenced(SqlSegmentEnums.INSERT_IF_TAG_SEG.getValue(),StringUtils.underline2Camel(sourceColumnName,true));
-        }
-        if (SqlSegmentEnums.UPDATE_IF_TAG_SEG.getKey().equals(ifTagType)){
-            return StringUtils.replaceSequenced(SqlSegmentEnums.UPDATE_IF_TAG_SEG.getValue(),StringUtils.underline2Camel(sourceColumnName,true),sourceColumnName);
-        }
-        return "";
-    }
-
-    private String getMapperLocation(String tableName){
-        return mapperLocation+"."+StringUtils.underline2Camel(tableName,false)+"Mapper";
-    }
-
-    private String getEntityLocation(String tableName){
-        return entityLocation+"."+StringUtils.underline2Camel(tableName,false);
-    }
 
 }
--- land-dal/src/main/java/com/captain/template/utils/FileUtils.java
+++ land-dal/src/main/java/com/captain/template/utils/FileUtils.java
@@ -7,16 +7,20 @@ import java.io.IOException;
  * Created by wurz on 2018/6/13.
  */
 public class FileUtils {
-    public static File createFile(String path) throws IOException {
-        File file = new File(path);
+    public static File createFile(String pathAndFileName) throws IOException {
+        File file = new File(pathAndFileName);
         if(!file.exists()){
             file.getParentFile().mkdirs();
         }
+
         file.createNewFile();
         return file;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileUtils.createFile(FileUtils.class.getResource("").getPath()+"test.xml");
         System.out.println(FileUtils.class.getResource("").getPath());
     }
+
+
 }
